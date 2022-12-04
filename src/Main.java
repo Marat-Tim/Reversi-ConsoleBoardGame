c7e50252ea84447a0de195f65e27c0d20d1ae179
Marat-Tim <rurikcat@gmail.com>
2022-12-04 22:28:43 +0300

    коммит на всякий случай

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("a = %s", null);
         Player first = new HumanPlayer();
         Player second = new HumanPlayer();
         Game game = new Game(first, second);
